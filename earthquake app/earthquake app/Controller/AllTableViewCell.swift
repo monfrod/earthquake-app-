ab3bd15ac165c0f1ae6fbb8e066f3d92d0b662cc
monfrod <iunus.lalazov.05@gmail.com>
2024-05-03 01:54:18 +0500

    ..

---
+++ earthquake app/earthquake app/Controller/AllTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  AllTableViewCell.swift
+//  earthquake app
+//
+//  Created by yunus on 03.05.2024.
+//
+
+import UIKit
+
+class AllTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+
+}
--- earthquake app/earthquake app/Controller/AllViewController.swift
+++ earthquake app/earthquake app/Controller/AllViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class AllViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
+}
+extension AllViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "allCell", for: indexPath) as! AllTableViewCell
+        cell.titleLabel.text = "hEllo"
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140.0
+    }
+    
+    
 }
